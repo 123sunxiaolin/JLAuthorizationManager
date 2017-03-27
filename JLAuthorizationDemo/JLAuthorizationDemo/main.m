11abbce19f33cf6581fdf56868b2c43632453cf2
Jack_lin <401788217@qq.com>
2017-03-27 23:07:04 +0800

    init project

---
+++ JLAuthorizationDemo/JLAuthorizationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JLAuthorizationDemo
+//
+//  Created by Sunxiaolin on 17/3/27.
+//  Copyright © 2017年 com.jack.lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
